e683dda5341dcca1266a465444aae788c897f023
nielshn <niel.shn08@gmail.com>
2025-06-12 19:33:38 +0700

    wine workflow

--- scripts/upload_to_drive.py
+++ scripts/upload_to_drive.py
@@ -1,22 +1,23 @@
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
 import os
 
-# GANTI dengan folder ID Drive kamu
+SERVICE_ACCOUNT_FILE = 'service_account.json'
 FOLDER_ID = "1ycZSe3JOL0avT2peCq8ubQoZB97Mwox4"
 ARTIFACTS_DIR = "model_output"
 
-gauth = GoogleAuth()
-gauth.LoadClientConfigFile("scripts/client_secrets.json")
-gauth.LocalWebserverAuth()
-drive = GoogleDrive(gauth)
+SCOPES = ['https://www.googleapis.com/auth/drive.file']
+credentials = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+service = build('drive', 'v3', credentials=credentials)
 
 for root, dirs, files in os.walk(ARTIFACTS_DIR):
     for filename in files:
         file_path = os.path.join(root, filename)
-        print(f"Uploading {file_path} to GDrive folder ...")
-        gfile = drive.CreateFile(
-            {'title': filename, 'parents': [{'id': FOLDER_ID}]})
-        gfile.SetContentFile(file_path)
-        gfile.Upload()
-        print(f"Uploaded {filename}")
+        file_metadata = {'name': filename, 'parents': [FOLDER_ID]}
+        media = MediaFileUpload(file_path, resumable=True)
+        file = service.files().create(
+            body=file_metadata, media_body=media, fields='id'
+        ).execute()
+        print(f"Uploaded {filename} with ID: {file.get('id')}")
